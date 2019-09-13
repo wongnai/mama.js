c48c95013e53b657fdd33d2294760d6e710dafe1
winChawakorn <chawakorn.su@ku.th>
2019-09-13 16:49:06 +0700

    Initialize project with rollup typescript

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import { terser } from 'rollup-plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
+import includepaths from 'rollup-plugin-includepaths'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es' // the preferred format
+    },
+    {
+      file: pkg.browser,
+      format: 'iife',
+      name: 'wongai'
+    }
+  ],
+  plugins: [
+    typescript(),
+    terser(),
+    includepaths({
+      path: ['./src'],
+      extensions: ['.ts']
+    })]
+}
