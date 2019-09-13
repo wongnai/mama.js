1d975ab04ece4b12988cca5c33803d8a5f0b6a5e
voraton.l <voraton@wongnai.com>
2019-09-13 18:42:52 +0700

    Make it compile  cli correctly

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,19 @@
-import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
 import includepaths from 'rollup-plugin-includepaths'
-import shebang from 'rollup-plugin-preserve-shebang'
 import pkg from './package.json'
-import path from 'path'
 
 export default {
   input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-    {
-      file: pkg.browser,
-      format: 'iife',
-      name: 'wongai',
-    },
-  ],
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+    banner: '#!/usr/bin/env node',
+  },
   plugins: [
-    shebang(),
     typescript(),
-    terser(),
     includepaths({
       path: ['./src'],
       extensions: ['.ts'],
-    }),
+    })
   ],
 }
