052984393ac63299d92cdf5db42e8630f2f9e044
dacharat <dacharat.p@ku.th>
2019-10-25 17:37:41 +0700

    remove console

--- src/components/FirebaseInitializer.vue
+++ src/components/FirebaseInitializer.vue
@@ -1,30 +1,28 @@
 <template>
-	<v-container>
-		<v-card class="d-flex pa-2">
-			<v-card-text>
-				<v-textarea
-					name="firebaseConfig"
-					label="Firebase Config"
-					v-model="firebaseConfig"
-				></v-textarea>
-				<v-btn color="orange" class="white--text" v-on:click="setConfig">Initialize Firebase App!</v-btn>
-			</v-card-text>
-		</v-card>
-	</v-container>
+  <v-container>
+    <v-card class="d-flex pa-2">
+      <v-card-text>
+        <v-textarea name="firebaseConfig" label="Firebase Config" v-model="firebaseConfig"></v-textarea>
+        <v-btn color="orange" class="white--text" v-on:click="setConfig">Initialize Firebase App!</v-btn>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 <script>
 export default {
   name: "FirebaseInitializer",
   methods: {
-	  setConfig: function() {
-		  const goodConfig = this.firebaseConfig.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ')
-		  console.log(goodConfig)
-	  }
+    setConfig: function() {
+      const goodConfig = this.firebaseConfig.replace(
+        /(['"])?([a-z0-9A-Z_]+)(['"])?:/g,
+        '"$2": '
+      );
+    }
   },
   data() {
-	  return {
-		  firebaseConfig: JSON.stringify({}),
-	  }
+    return {
+      firebaseConfig: JSON.stringify({})
+    };
   }
 };
 </script>
