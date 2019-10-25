dbe9df306f31da57a0fbad6a2bbf17942fdf9d38
voraton.l <voraton@wongnai.com>
2019-10-25 17:08:41 +0700

    Firebase Init but not init

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,24 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="blue--text text--darken-3">Wongnai</span>
+        <span class="font-weight-light orange--text"> Firebase Playground</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <FirebaseInitializer />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import FirebaseInitializer from './components/FirebaseInitializer'
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  },
-  data: () => ({
-    //
-  })
+    FirebaseInitializer,
+  }
 };
 </script>
--- src/components/FirebaseInitializer.vue
+++ src/components/FirebaseInitializer.vue
@@ -1,13 +1,30 @@
 <template>
-  <v-textarea
-    name="input-7-1"
-    label="Default style"
-    value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-    hint="Hint text"
-  ></v-textarea>
+	<v-container>
+		<v-card class="d-flex pa-2">
+			<v-card-text>
+				<v-textarea
+					name="firebaseConfig"
+					label="Firebase Config"
+					v-model="firebaseConfig"
+				></v-textarea>
+				<v-btn color="orange" class="white--text" v-on:click="setConfig">Initialize Firebase App!</v-btn>
+			</v-card-text>
+		</v-card>
+	</v-container>
 </template>
 <script>
 export default {
-  name: "FirebaseInitializer"
+  name: "FirebaseInitializer",
+  methods: {
+	  setConfig: function() {
+		  const goodConfig = this.firebaseConfig.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ')
+		  console.log(goodConfig)
+	  }
+  },
+  data() {
+	  return {
+		  firebaseConfig: JSON.stringify({}),
+	  }
+  }
 };
 </script>
