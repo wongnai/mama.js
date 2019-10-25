41838c85bce22057f56508fa887719a8d4999bca
voraton.l <voraton@wongnai.com>
2019-10-25 16:32:47 +0700

    Add Vuetify :P

---
+++ src/components/FirebaseInitializer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-textarea
+    name="input-7-1"
+    label="Default style"
+    value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+    hint="Hint text"
+  ></v-textarea>
+</template>
+<script>
+export default {
+  name: "FirebaseInitializer"
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <FirebaseInitializer />
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import FirebaseInitializer from "@/components/FirebaseInitializer.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    FirebaseInitializer
   }
 };
 </script>
